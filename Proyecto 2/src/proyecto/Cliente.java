e8818e3e538726c2ecd24a53ff5c197b1fe340d9
Adrimora18 <136401595+Adrimora18@users.noreply.github.com>
2023-08-11 06:40:20 -0600

    Add files via upload

---
+++ Proyecto 2/src/proyecto/Cliente.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jappsemana11;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Cliente {
+    
+    String IdeCliente;
+    String NomCliente;
+    char TipoCliente;
+
+    public String getIdeCliente() {
+        return IdeCliente;
+    }
+
+    public void setIdeCliente(String IdeCliente) {
+        this.IdeCliente = IdeCliente;
+    }
+
+    public String getNomCliente() {
+        return NomCliente;
+    }
+
+    public void setNomCliente(String NomCliente) {
+        this.NomCliente = NomCliente;
+    }
+
+    public char getTipoCliente() {
+        return TipoCliente;
+    }
+
+    public void setTipoCliente(char TipoCliente) {
+        this.TipoCliente = TipoCliente;
+    }
+    
+}
